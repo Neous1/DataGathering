5fe6c752f1e8959b9a5964bb6362c7e896dceb08
Neous1 <Neous1@users.noreply.github.com>
2018-02-04 13:50:36 -0500

    create a DataModel class to model our table

---
+++ WebDataGathering/WebDataGathering/Models/DataModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebDataGathering.Models
+{
+    public class DataModel
+    {
+        public int Id { get; set; }
+        public DateTime PullTime { get; set; }
+        public string Symbol { get; set; }
+        public string LastPrice { get; set; }
+        public string ChangePerc { get; set; }
+        public string Volume { get; set; }
+        public string VolumeAvg { get; set; }
+    }
+}
